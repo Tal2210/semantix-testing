17dd94be7338bf1e11c57972b13b2202b3eab7b5
GalPaz <galpaz2210@gmail.com>
2024-08-15 16:08:59 +0300

    first commit4

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,21 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "סמנטיקס - חיפוש AI לתוצאות מדויקות בעסק שלך",
+  title: "סמנטיקס - חיפוש סמנטי מבוסס AI לתוצאות מדויקות בעסק שלך",
   description: " סמנטיקס - התאמה מושלמת וחכמה בין חיפוש למוצר",
+  icons: {
+    icon: "/logo-semantix.png",
+  },
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="he" dir="rtl">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/logo-semantix.png" type="image/png" />
+      </head>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <div className="flex-grow relative overflow-hidden">
           <div className="absolute top-0 right-0 w-1/2 h-full bg-purple-500 opacity-10 -skew-x-12 transform origin-top-right"></div>
@@ -27,7 +35,7 @@ export default function RootLayout({ children }) {
           
             </ul>*/}
           
-             <Link href="/"> <img src="/semantix black-cutout.png" alt="לוגו סמנטיקס" width={200} height={150} /> </Link>
+             <Link href="/"> <img src="/semantix black-cutout.png" alt="לוגו סמנטיקס- חיפוש סמנטי לעסק שלך" width={200} height={150} /> </Link>
             </nav>
           </header>
 
